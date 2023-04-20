d0aff5527f6b0b98716fbcc317950a9c177684fc
CP-ThinkDigital <coderplaysmarketing@gmail.com>
2023-04-20 15:32:53 +0530

    Added Rxjs code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
+const { exposeIpcMainRxStorage } = require("rxdb/plugins/electron");
+const { getRxStorageMemory } = require("rxdb/plugins/storage-memory");
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
@@ -30,7 +33,14 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", async function () {
+  exposeIpcMainRxStorage({
+    key: "udeler-pro",
+    storage: getRxStorageMemory(),
+    ipcMain: ipcMain,
+  });
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
