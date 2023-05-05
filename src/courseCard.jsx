de72dee4e0930bb09efbfddbbed81691dc923a1f
Sathishkumar R <rsathishtechit@gmail.com>
2023-05-05 22:57:35 +0530

    Added pause/resume action to course download

--- src/courseCard.jsx
+++ src/courseCard.jsx
@@ -13,6 +13,14 @@ export default function CourseCard({ course }) {
 
   let { token, url } = useContext(UdemyContext);
 
+  const pauseDownload = () => {
+    downloader._downloads.forEach((dl) => dl.stop());
+  };
+
+  const resumeDownload = () => {
+    downloader._downloads.forEach((dl) => dl.resume());
+  };
+
   const fetchCourseData = async () => {
     await fetch(
       `https://www.udemy.com/api-2.0/courses/${
@@ -28,6 +36,7 @@ export default function CourseCard({ course }) {
       .then(({ results }) => {
         const courseData = {};
         let current;
+        let num = 1;
         results.forEach((item, index) => {
           if (item._class === "chapter") {
             // debugger;
@@ -63,24 +72,57 @@ export default function CourseCard({ course }) {
                 .then((res) => res.json())
                 .then(({ asset }) => {
                   let lecturePath = join(sectionPath, asset.filename);
-                  mkdirp(sectionPath).then(() => {
-                    const download = downloader.download(
-                      asset.media_sources[0]["src"],
-                      lecturePath
-                    );
-                    download.setRetryOptions({
-                      maxRetries: 3, // Default: 5
-                      retryInterval: 3000, // Default: 2000
-                    });
+                  if (asset.media_sources[0].type === "video/mp4") {
+                    mkdirp(sectionPath).then(() => {
+                      const download = downloader.download(
+                        asset.media_sources[0]["src"],
+                        lecturePath
+                      );
+                      download.setRetryOptions({
+                        maxRetries: 3, // Default: 5
+                        retryInterval: 3000, // Default: 2000
+                      });
 
-                    // Set download options
-                    download.setOptions({
-                      threadsCount: 5, // Default: 2, Set the total number of download threads
-                      timeout: 5000, // Default: 5000, If no data is received, the download times out (milliseconds)
-                      range: "0-100", // Default: 0-100, Control the part of file that needs to be downloaded.
+                      // Set download options
+                      download.setOptions({
+                        threadsCount: 6, // Default: 2, Set the total number of download threads
+                        timeout: 5000, // Default: 5000, If no data is received, the download times out (milliseconds)
+                        range: "0-100", // Default: 0-100, Control the part of file that needs to be downloaded.
+                      });
+
+                      var timer = setInterval(function () {
+                        if (download.status == -2) {
+                          console.log("Download " + num + " stopped.");
+                        } else if (download.status == -3) {
+                          console.log("Download " + num + " destroyed.");
+                        }
+
+                        if (
+                          download.status === -1 ||
+                          download.status === 3 ||
+                          download.status === -3
+                        ) {
+                          clearInterval(timer);
+                          timer = null;
+                        }
+                      }, 1000);
+                      download.start();
+                      download.on("error", function (error) {
+                        console.log("EVENT - Download " + error + " error !");
+                        console.log(download.error);
+                      });
+                      download.on("progress", function (progress) {
+                        console.log(
+                          "EVENT - Download " + num + " progress " + progress
+                        );
+                      });
+                      download.on("end", function () {
+                        console.log(
+                          "EVENT - Download " + num + " end " + download.status
+                        );
+                      });
                     });
-                    download.start();
-                  });
+                  }
                 });
             }
           }
@@ -92,7 +134,7 @@ export default function CourseCard({ course }) {
     <>
       <div className="flex gap-x-4">
         <img
-          className="h-52 w-4/12 w-full border border-gray-300 bg-white text-gray-300"
+          className="h-32 w-auto border border-gray-300 bg-white text-gray-300"
           src={course.image_480x270}
           alt=""
         />
@@ -104,6 +146,19 @@ export default function CourseCard({ course }) {
               className=" rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
             >
               Download
+            </button>
+
+            <button
+              onClick={pauseDownload}
+              className=" rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+            >
+              Pause
+            </button>
+            <button
+              onClick={resumeDownload}
+              className=" rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+            >
+              Resume
             </button>
           </div>
         </div>
